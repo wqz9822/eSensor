96778fa7a443e7ed83331119d1ca0475c1fb549a
Wang George <wqz9822@gmail.com>
2015-01-22 22:46:24 +0800

    Global variables and functions

---
+++ Common.h
@@ -0,0 +1,33 @@
+/* bit masks */
+#define BIT0    0x01
+#define BIT1    0x02
+#define BIT2    0x04
+#define BIT3    0x08
+#define BIT4    0x10
+#define BIT5    0x20
+#define BIT6    0x40
+#define BIT7    0x80
+#define BIT8    0x100
+#define BIT9    0x200
+#define BIT10   0x400
+#define BIT11   0x800
+#define BIT12   0x1000
+#define BIT13   0x2000
+#define BIT14   0x4000
+#define BIT15   0x8000
+
+
+
+//=====================全局变量============================//
+ float temperature,humidity;
+ int ButtonChoice;
+
+
+//=====================函数定义============================//
+
+
+void delay_ms(int t);//延时若干毫秒
+
+void delay_little(void); //短延时
+
+
